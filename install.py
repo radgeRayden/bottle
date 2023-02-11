a75b2f80e6a933f5f4438c6eacc0dda2b64c683a
Westerbly Snaydley <westerbly@gmail.com>
2023-02-11 19:01:42 -0300

    script for installing a new bottle project template

---
+++ install.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import urllib.request
+
+bottle_src = os.path.dirname(os.path.realpath(__file__))
+install_dir = os.getcwd()
+bottle_dst = f"{install_dir}/lib/scopes/packages/bottle"
+
+shutil.copytree(f"{bottle_src}/recipes", f"{install_dir}/recipes")
+
+os.makedirs(bottle_dst, 0o777)
+shutil.copytree(f"{bottle_src}/src", f"{bottle_dst}/src")
+shutil.copyfile(f"{bottle_src}/init.sc", f"{bottle_dst}/init.sc")
+shutil.copyfile(f"{bottle_src}/LICENSE", f"{bottle_dst}/LICENSE")
+shutil.copyfile(f"{bottle_src}/README.md", f"{bottle_dst}/README.md")
+
+eo_url = "https://hg.sr.ht/~duangle/majoreo/raw/eo?rev=tip"
+urllib.request.urlretrieve(eo_url, "eo")
+os.system("python eo init; python eo install -y wgpu-native-release sdl2 stb")
