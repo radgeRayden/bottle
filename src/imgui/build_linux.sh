9fb55ccba5c5c86f2297b6ced110392df09ef8a2
Westerbly Snaydley <westerbly@gmail.com>
2023-06-05 04:43:07 -0300

    patch SDL2 soname on linux to match the libname

--- src/imgui/build_linux.sh
+++ src/imgui/build_linux.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-pushd "./lib"
-for lib in *.so; do
-    SONAME=$(readelf -d $lib | rg -e ".+Library soname: \[" -r '$1' | rev | cut --complement -c -1 | rev)
-    mv $lib $SONAME
-done
-popd
-
 g++ -o libcimgui.so -shared -O2 -fPIC -DIMGUI_IMPL_API='extern "C"' cimgui.cpp imgui/imgui.cpp imgui/imgui_demo.cpp imgui/imgui_draw.cpp imgui/imgui_tables.cpp imgui/imgui_widgets.cpp imgui/backends/imgui_impl_wgpu.cpp imgui/backends/imgui_impl_sdl2.cpp -I./include -I./include/SDL2 -I./imgui/backends -I./imgui/ -L./lib -lSDL2 -Wl,-rpath '-Wl,$ORIGIN' -l:libwgpu_native.so -Wl,--export-dynamic
