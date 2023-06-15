fe42b60b155c2b098bb870d80a703e76d01ef099
Westerbly Snaydley <westerbly@gmail.com>
2023-06-15 15:05:31 -0300

    demo build script copies assets and zips output

--- demos/build.sh
+++ demos/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env sh
 
+rm -r dist
+
 scopes -e ./build.sc ".gpu.hello-triangle"
 scopes -e ./build.sc ".gpu.buffers"
 scopes -e ./build.sc ".gpu.textures"
 scopes -e ./build.sc ".plonk.sprites"
 scopes -e ./build.sc ".snake"
+
+cp ./gpu/textures/linus.jpg ./dist/bin
+cp ./plonk/sprites/_Run.png ./dist/bin
+cp ./snake/snake.png ./dist/bin
+cd ./dist/bin
+zip ../../dist.zip ./*
+cd ../..
