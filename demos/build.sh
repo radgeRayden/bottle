59d8a835ff1b5db935ba809d4797cbc0e516bad3
Westerbly Snaydley <westerbly@gmail.com>
2025-10-13 12:55:23 -0500

    demos: improve platform check in build script

--- demos/build.sh
+++ demos/build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 SCRIPT_SRC=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_SRC
+[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] && IS_MINGW_PLATFORM=1 || IS_MINGW_PLATFORM=0
 
 rm -rf ./dist ./bottle-demos bottle-demos.zip
 mkdir ./dist
@@ -12,7 +13,7 @@ mkdir ./dist/obj
 DEMO_REGEXP="^[^#].+"
 if [ $# -gt 0 ]; then
     DEMOS=$*
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+elif [ $IS_MINGW_PLATFORM == 1 ]; then
     DEMOS="$(cat demo-list.txt | grep -E $DEMO_REGEXP | tr '\n' ' ' | tr '\r' ' ')"
 else
     DEMOS="$(cat demo-list.txt demo-list-linux.txt | grep -E $DEMO_REGEXP | tr '\n' ' ')"
@@ -25,7 +26,7 @@ popd
 cp -r ./assets ./dist/bin/
 mv ./dist/bin ./bottle-demos
 
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+if [ "$IS_MINGW_PLATFORM" == 1 ]; then
     mv ./bottle-demos/physfs.dll ./bottle-demos/libphysfs.dll
     cp /mingw64/bin/libstdc++-6.dll ./bottle-demos/
     cp /mingw64/bin/libgcc_s_seh-1.dll ./bottle-demos/
