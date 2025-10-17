8347d0a4aeb25d7a09d892880a7d28d048afe267
Westerbly Snaydley <westerbly@gmail.com>
2025-10-16 21:04:19 -0500

    autoformat dodo.py

--- dodo.py
+++ dodo.py
@@ -2,59 +2,76 @@ import platform
 import os
 from doit.tools import LongRunning
 
-DOIT_CONFIG = {'default_tasks': ['build_demos']}
+DOIT_CONFIG = {"default_tasks": ["build_demos"]}
 FILTER_ANSI = os.getenv("DOIT_FILTER_ANSI")
+
 
 def cmd(cmd):
     if platform.system() == "Linux":
         prefix = "MANGOHUD=1 MANGOHUD_CONFIG=horizontal,gpu_name,gpu_power,present_mode,resolution,vram,vulkan_driver,position=bottom-left bash -c"
     else:
         prefix = "bash -c"
-    return f"{prefix} \"{cmd}\""
+    return f'{prefix} "{cmd}"'
+
 
 def task_eo():
     return {
-        'actions': [cmd("wget -O eo https://hg.sr.ht/~duangle/majoreo/raw/eo?rev=tip"), cmd("chmod +x eo")],
-        'targets': ["eo"],
-        'uptodate': [True]
+        "actions": [
+            cmd("wget -O eo https://hg.sr.ht/~duangle/majoreo/raw/eo?rev=tip"),
+            cmd("chmod +x eo"),
+        ],
+        "targets": ["eo"],
+        "uptodate": [True],
     }
+
 
 def is_eo_initialized():
     return os.path.isdir(".eo/")
 
+
 def task_eo_init():
     return {
-        'actions': [cmd("./eo init && touch ./.eo/initialized")],
-        'targets': [".eo/initialized"],
-        'uptodate': [is_eo_initialized],
+        "actions": [cmd("./eo init && touch ./.eo/initialized")],
+        "targets": [".eo/initialized"],
+        "uptodate": [is_eo_initialized],
     }
 
+
 bootstrap = ".eo/installed/bootstrap"
+
+
 def task_bootstrap():
     return {
-        'verbosity': 2,
-        'actions': [cmd("yes | ./eo sync && ./eo install -y bootstrap")],
-        'targets': [bootstrap],
-        'uptodate': [False],
+        "verbosity": 2,
+        "actions": [cmd("yes | ./eo sync && ./eo install -y bootstrap")],
+        "targets": [bootstrap],
+        "uptodate": [False],
     }
+
 
 def task_force_bootstrap():
     return {
-        'verbosity': 2,
-        'actions': [cmd("rm -rf .eo lib include"), cmd("./eo init"), cmd("./eo install -y bootstrap")],
-        'uptodate': [False],
-        'file_dep': ["eo"],
+        "verbosity": 2,
+        "actions": [
+            cmd("rm -rf .eo lib include"),
+            cmd("./eo init"),
+            cmd("./eo install -y bootstrap"),
+        ],
+        "uptodate": [False],
+        "file_dep": ["eo"],
     }
 
+
 def read_demo_list(path):
-    with open(path, 'r') as file:
-        demo_list = file.read().split('\n')
+    with open(path, "r") as file:
+        demo_list = file.read().split("\n")
         return [demo for demo in demo_list if demo and (not demo.startswith("#"))]
 
 
 demos = read_demo_list("demos/demo-list.txt")
 if platform.system() == "Linux":
     demos += read_demo_list("demos/demo-list-linux.txt")
+
 
 def demo_exe_name(name):
     sanitized_name = name.replace(".", "_")
@@ -64,35 +81,40 @@ def demo_exe_name(name):
         ext = ""
     return f"demo_{sanitized_name}{ext}"
 
+
 def demo_cmd(name):
     return cmd(f"RUST_BACKTRACE=1 scopes -e run.sc {name}")
 
-def task_demos ():
+
+def task_demos():
     for name in demos:
         yield {
-            'basename': f"demo.{name}",
-            'actions': [LongRunning(demo_cmd(name))],
-            'file_dep': [bootstrap],
-            'uptodate': [False]
+            "basename": f"demo.{name}",
+            "actions": [LongRunning(demo_cmd(name))],
+            "file_dep": [bootstrap],
+            "uptodate": [False],
         }
+
 
 def live_demo_cmd(name):
     return cmd(f"RUST_BACKTRACE=1 scopes -e run.sc {name} --live-reload")
 
-def task_live_code ():
+
+def task_live_code():
     for name in demos:
         yield {
-            'basename': f"live.{name}",
-            'actions': [LongRunning(live_demo_cmd(name))],
-            'file_dep': [bootstrap],
-            'uptodate': [False]
+            "basename": f"live.{name}",
+            "actions": [LongRunning(live_demo_cmd(name))],
+            "file_dep": [bootstrap],
+            "uptodate": [False],
         }
+
 
 def task_build_demos():
     return {
-        'verbosity': 2,
-        'actions': [cmd("./demos/build.sh")],
-        'targets': ["demos/bottle-demos.zip"],
-        'file_dep': [bootstrap],
-        'uptodate': [False]
+        "verbosity": 2,
+        "actions": [cmd("./demos/build.sh")],
+        "targets": ["demos/bottle-demos.zip"],
+        "file_dep": [bootstrap],
+        "uptodate": [False],
     }
